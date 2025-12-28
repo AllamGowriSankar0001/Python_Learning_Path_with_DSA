cdaf49d5de3fc1b4134f51a8c26c4d58a26ea876
AllamGowriSankar0001 <allamgowrisankar0001@gmail.com>
2025-12-28 16:00:16 +0530

    added the varibales

--- 1.Python_Fundamentals/2. Variables/variables.py
+++ 1.Python_Fundamentals/2. Variables/variables.py
@@ -7,3 +7,6 @@ print(name, age, salary) # This will print the values of the variables to the co
 # we can have _ in variable name like this: my_name, my_age, my_salary, etc.    
 # we can have numbers in variable name like this: name1, age2, salary3, etc.
 # we cant have space in variable name like this: my name, my age, my salary, etc.
+name,age,salary = "John",20,20000.00 # This is a multiple assignment,
+print(name,age,salary) # This will print the values of the variables to the console,
+# we can have multiple variables in one line like this: name,age,salary = "John",20,20000.00
