78281f7dee8b234d184443e537b0c29a66b7d91f
AllamGowriSankar0001 <allamgowrisankar0001@gmail.com>
2025-12-28 15:45:32 +0530

    added python intro and setup

---
+++ 1.Python_Fundamentals/1. Python_Intro/hello_world.py
@@ -0,0 +1,3 @@
+print("Hello, World!") # This will print "Hello, World!" to the console,
+                       # Print is a built-in function in Python,
+                       # That prints the string to the console.
