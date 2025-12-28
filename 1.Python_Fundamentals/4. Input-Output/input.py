01b4278ec244ae07e293d779a3d8d5b4ea25b938
AllamGowriSankar0001 <allamgowrisankar0001@gmail.com>
2025-12-28 16:31:50 +0530

    added input/output

--- 1.Python_Fundamentals/4. Input-Output/input.py
+++ 1.Python_Fundamentals/4. Input-Output/input.py
@@ -1,3 +1,6 @@
+x = input("Enter something: ")
+print(x)
+print(type(x)) #<class 'str'> all inputs are strings
 a = str(input("Enter your name: "))  #only strings are accepted
 print(a) 
 b = int(input("Enter your age: ")) #only numbers are accepted
@@ -10,3 +13,54 @@ print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+y = int(input("Enter a: "))
+z = int(input("Enter b: "))
+sum_result = y + z
+print(sum_result) #this will print the sum of a and b
+print(type(sum_result)) #this will print the type of sum_result which is int
+
+# we can also use multiple inputs in one line like this:
+p,q,r = map(int,input("enter three numbers: ").split())
+print(p+q+r)
+print(type(p+q+r))
+
+#Separator parameter
+print("Hello", "World", sep="*") #this will print Hello*World
+print("Hello", "World", sep="\n") #this will print Hello\nWorld in new line
+print("Hello", "World", sep="\t") #this will print Hello\tWorld in tab
+print("Hello", "World", sep="\n\t") #this will print Hello\n\tWorld in new line and tab
+
+#End parameter
+print("Hello", "World", end=" ") #this will print Hello World in same line
+print("Hello", "World", end="...") #this will print Hello...World in same line
+
+#Formatted output/f-strings
+name = "John"
+age = 20
+print(f"Name: {name}, Age: {age}") #this will print Name: John, Age: 20
+print(f"Name: {name}, Age: {age}") #this will print Name: John, Age: 20
+
+#Escape characters
+print("Hello\nWorld") #this will print Hello World in new line
+print("Hello\tWorld") #this will print Hello World in tab space
+print("Hello\"World") #this will print Hello"World in double quotes
+print("Hello\\World") #this will print Hello\World in backslash
+
+#Common errors
+#Error 1: Forgetting Type Conversion
+age = input("Enter your age: ")
+print(age+10) #this will print the age plus 10 which is a string
+print(type(age)) #this will print the type of age which is a string
+print(int(age)+10) #this will print the age plus 10 which is a integer
+print(type(int(age)+10)) #this will print the type of int(age)+10 which is a integer
+
+#Error 2: Using + with String and Number
+age = 20
+print("Age: " + age) #this will print the age which is a number
+
+#Error 3: Wrong Indentation
+name = input("Enter name: ")
+    print(name) #this will print the name which is a string
+
+#Error 4: Using print() without parentheses
+print("Hello") #this will print Hello which is a string
---
+++ 1.Python_Fundamentals/4. Input-Output/simple-calculator.py
@@ -0,0 +1,11 @@
+# Get inputs
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+sum = num1 + num2
+print(f"Sum: {sum}") #this will print the sum of num1 and num2
+difference = num1 - num2
+print(f"Difference: {difference}") #this will print the difference of num1 and num2
+product = num1 * num2
+print(f"Product: {product}") #this will print the product of num1 and num2
+quotient = num1 / num2
+print(f"Quotient: {quotient}") #this will print the quotient of num1 and num2
