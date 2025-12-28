4abe3130290534d6cc5397d1129ca3aeea545fa3
AllamGowriSankar0001 <allamgowrisankar0001@gmail.com>
2025-12-28 15:59:09 +0530

    added the varibales

---
+++ 1.Python_Fundamentals/2. Variables/variables.py
@@ -0,0 +1,9 @@
+name = "John" # This is a string variable,
+age = 20 # This is an integer variable,
+salary = 20000.00 # This is a float variable,
+print(name, age, salary) # This will print the values of the variables to the console,
+#we will be learning more about data types in the next section.
+#In this way we can store different types of data in variables.
+# we can have _ in variable name like this: my_name, my_age, my_salary, etc.    
+# we can have numbers in variable name like this: name1, age2, salary3, etc.
+# we cant have space in variable name like this: my name, my age, my salary, etc.
